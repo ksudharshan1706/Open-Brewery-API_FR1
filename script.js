2a6ea7735edddbfc12dd97a09c0fb6f9291c63a2
k.sudharshan <kamavaramsudharshan1706@gmail.com>
2022-11-12 20:51:07 +0530

    code update

--- script.js
+++ script.js
@@ -12,7 +12,6 @@ let newElement = document.createElement('div')
 document.body.appendChild(newElement)
 
 const inputTag = document.getElementById("text-inp")
-// inputTag.setAttribute('onkeyup',"SearchForBook()")
 
 const displayDiv = document.getElementById("displaydiv")
 displayDiv.className = "m-3"
@@ -22,26 +21,7 @@ displayDiv.style.backgroundColor = "white"
 displayDiv.style.display = "None"
 displayDiv.style.border = "2px solid"
 const table = document.createElement('table')
-// table.classList.add("table table-stripped")
-table.className = "table table-hover mt-3"
-const tablehead = document.createElement('thead')
-tablehead.className="table table-dark"
-const tablebody = document.createElement('tbody')
-tablebody.id = "tablebody"
-const tablerow = document.createElement('tr') 
-const headFileds =  ['brewaryName','BrewaryInfo']
-headFileds.map((data)=>{
-  const headdata = document.createElement('td')  
-  headdata.innerText = data
-  tablerow.appendChild(headdata)
-})
-tablehead.append(tablerow)
-table.append(tablehead,tablebody)
-document.body.append(table)
-table.style.display = "none"
-
 const getbrewery = async ()=>{
-    // const BreweryArea = document.getElementById('Brewery-area')
     const response = await fetch('https://api.openbrewerydb.org/breweries')
     console.log(response);
   const message = await response.json()
@@ -51,16 +31,7 @@ const getbrewery = async ()=>{
 
   console.log(maxInd)
   globalData.forEach(({name,brewery_type,street,phone,website_url},index)=>{
-    dataDict[name] =  `${brewery_type}\n${street}\n${phone}\n${website_url}\n`;
-    // console.log(dataDict[name])
-
-      const innertr = document.createElement('tr');
-      const innername = createElements(name,'td');
-
-      const innerIsbn = `${brewery_type}\n${street}\n${phone}\n${website_url}`
-      // console.log(innerIsbn)
-      innertr.append(innername,innerIsbn);
-      tablebody.append(innertr)
+    dataDict[name] =  `${brewery_type}\n${street}\n${phone}\n${website_url}\n`;  
 })
 
 }
@@ -70,31 +41,10 @@ getbrewery()
 const submitbtn = document.querySelector("button")
 submitbtn.addEventListener('click',(e)=>{
   e.preventDefault();
+  console.log("here",dataDict)
   console.log(inputTag.value,dataDict[inputTag.value])
   displayDiv.style.display = "block"
-  displayDiv.className="simple-transition"
+  // displayDiv.style.transition ="all 1000ms ease";
+  displayDiv.classList.add("simple-transition")
   displayDiv.innerText = dataDict[inputTag.value]
 })
-// const getBreweryDetails = () => {
-
-// }
-
-// function SearchForBook() {
-//   var input, filter, table, tr, td, i, txtValue;
-//   input = document.getElementById("text-inp");
-//   filter = input.value.toUpperCase();
-//   table = document.getElementById("tablebody");
-//   console.log(table)
-//   tr = table.getElementsByTagName("tr");
-//   for (i = 0; i < tr.length; i++) {
-//     td = tr[i].getElementsByTagName("td")[0];
-//     if (td) {
-//       txtValue = td.textContent || td.innerText;
-//       if (txtValue.toUpperCase().indexOf(filter) > -1) {
-//         tr[i].style.display = "";
-//       } else {
-//         tr[i].style.display = "none";
-//       }
-//     }       
-//   }
-// }
